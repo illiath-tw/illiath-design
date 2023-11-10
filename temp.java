8cd77d9bbb2ec84085c6199d7a78785b4f7cd06a
Illiath <146264045+illiath-tw@users.noreply.github.com>
2023-11-09 16:21:17 -0800

    beartic
    this is when ehan gave me a beartic

---
+++ temp.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class temp {
+    public static void main(String args[])
+       {
+            
+            try (Scanner scan = new Scanner(System.in)) {
+                String bonk = scan.nextLine();
+                System.out.println("this is " + bonk);
+            }
+       }
+}
